23a4099b900407e9925911db1630fb5caf74dc4d
Caruy Chen <caruychen@gmail.com>
2022-02-11 13:51:00 +0200

    add workflow

---
+++ deploy_vogsphere.sh
@@ -0,0 +1,19 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    deploy_vogsphere.sh                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/02/11 13:49:13 by cchen             #+#    #+#              #
+#    Updated: 2022/02/11 13:50:50 by cchen            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+VOGSPHERE=$(ls | sed -n '/intra-uuid/p')
+
+rm -rf ./${VOGSPHERE}/*
+
+cp -r ./$REPO_NAME/network ./${VOGSPHERE}
+cp -r ./$REPO_NAME/system ./${VOGSPHERE}
+cp -r ./$REPO_NAME/scripts ./${VOGSPHERE}
